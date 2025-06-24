d855521533afe08676d5f148c16acd1009a5ffe9
ajithbharathi <ajithbharathi11@gmail.com>
2025-06-24 17:20:29 +0530

    initial commit

--- js/map.js
+++
@@ -1,7 +0,0 @@
-console.log('====MAP====');
-
-const nums = [1, 2, 3, 4];
-
-console.log(nums.map((n) => n * 2)); // [2, 4, 6, 8]
-
-console.log(nums.map((n) => n > 2)); // [false, false, true, true]
