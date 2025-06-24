56d1a69de8163eab0bdaf1f01081a5567ffd6cba
ajithbharathi <ajithbharathi11@gmail.com>
2025-06-24 17:17:50 +0530

    initial commit

--- js/eventloop.js
+++
@@ -1,6 +0,0 @@
-console.log('===== EVENT LOOP =====');
-
-console.log('a');
-setTimeout(() => console.log('set time out'));
-Promise.resolve(() => console.log('promise')).then((res) => res());
-console.log('b');
